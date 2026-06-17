12285f9067893cc1ec70a3001f2d406575233b9a
Diana <ddmitrieva098@example.com>
2026-06-17 17:26:35 +1100

    добавлено количество уроков

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,12 +4,17 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lessons_count()
+
     class Meta:
         model = Course
-        fields = ["name", "description"]
+        fields = ["id", "name", "description", "lessons_count"]
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = ["name", "description", "video_link", "course"]
+        fields = ["id", "name", "description", "video_link", "course"]
