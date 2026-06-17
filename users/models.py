37b588f6b7b85da08a8873bc6548a42d5cf789b1
Diana <ddmitrieva098@example.com>
2026-06-17 17:34:16 +1100

    создана модель оплаты

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -32,3 +34,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь",
+                             help_text="Выберите пользователя", related_name="payments")
+    date = models.DateField(verbose_name="Дата оплаты", auto_now_add=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Оплаченный курс",
+                               help_text="Выберите курс", blank=True, null=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="Оплаченный урок",
+                               help_text="Выберите урок", blank=True, null=True)
+    amount = models.PositiveIntegerField(verbose_name="Сумма оплаты", help_text="Введите сумму оплаты")
+
+    PAYMENT_METHODS = [
+        ("cash", "Наличные"),
+        ("transfer_to_account", "Перевод на счет"),
+    ]
+    payment_method = models.CharField(choices=PAYMENT_METHODS, max_length=50, verbose_name="Способ оплаты",
+                                      help_text="Выберите способ оплаты")
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return f'{self.user.name} за курс "{self.course.name if self.course else self.lesson.name}"'
