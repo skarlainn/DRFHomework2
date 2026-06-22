caeaffe0738ad5afb04f551eec4663eec8b95bcd
Diana <ddmitrieva098@example.com>
2026-06-22 23:05:50 +1100

    добавлен CRUD для пользователя

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListView, UserCreateAPIView
+from users.views import PaymentListView, UserCreateAPIView, UserListView, UserDetailView, UserUpdateView, UserDeleteView
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("", UserListView.as_view(), name="users"),
+    path("user/<int:pk>/", UserDetailView.as_view(), name="user"),
+    path("user/<int:pk>/update/", UserUpdateView.as_view(), name="update_user"),
+    path("user/<int:pk>/delete/", UserDeleteView.as_view(), name="delete_user"),
 ]
