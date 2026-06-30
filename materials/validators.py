f7236f37a8f4b7496c03dcf3bb31d7a16552b0ad
Diana <ddmitrieva098@example.com>
2026-06-30 17:59:50 +1100

    фиксация

--- materials/validators.py
+++ materials/validators.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+from urllib.parse import urlparse
+
+VALID_DOMAINS = ('youtube.com',)
 
 
-def validate_video_link(value):
-    if not value.startswith("https://www.youtube.com/"):
+def validate_video_link(value: str) -> None:
+    parsed_url = urlparse(value)
 
-        raise serializers.ValidationError(
-            "Неверная ссылка на видео. Добавьте ссылку на видео с YouTube"
-        )
+    if not all([
+        parsed_url.scheme == 'https',
+        parsed_url.netloc not in VALID_DOMAINS,
+    ]):
+        raise serializers.ValidationError("Неверная ссылка на видео")
