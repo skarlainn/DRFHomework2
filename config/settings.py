2eb3c54156b36e793cae37b1a45a095156e1cb88
Diana <ddmitrieva098@example.com>
2026-07-14 20:43:27 +1100

    добавлен docker-compose.yaml

--- config/settings.py
+++ config/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DATABASE_NAME"),
-        "USER": os.getenv("DATABASE_USER"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "HOST": os.getenv("DATABASE_HOST"),
-        "PORT": os.getenv("DATABASE_PORT", default="5432"),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
