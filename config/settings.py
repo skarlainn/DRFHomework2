0f380e53be13e3f0fdc72af36bfa152a71a1930b
Diana <ddmitrieva098@example.com>
2026-07-19 20:38:14 +1100

    фикс

--- config/settings.py
+++ config/settings.py
@@ -97,12 +97,12 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'test_db.sqlite3',
-         }
-     }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'test_db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
